e78965c3defb57c30d56dc82b19fad432cf6903d
Ameen Ahmed <kid_1194@hotmail.com>
2022-07-31 18:38:32 +0300

    Create v1.0.0

---
+++ frappe_better_select_control/config/docs.py
@@ -0,0 +1,11 @@
+"""
+Configuration for docs
+"""
+
+# source_link = "https://github.com/[org_name]/frappe_better_select_control"
+# docs_base_url = "https://[org_name].github.io/frappe_better_select_control"
+# headline = "App that does everything"
+# sub_heading = "Yes, you got that right the first time, everything"
+
+def get_context(context):
+    context.brand_html = "Frappe Better Select Control"
