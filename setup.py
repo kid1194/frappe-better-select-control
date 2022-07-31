73e4efe47021540238c5b0fd9787878e034e4bc2
Ameen Ahmed <kid_1194@hotmail.com>
2022-07-31 18:45:18 +0300

    Create v1.0.0

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
+# get version from __version__ variable in erpnext_fiscal_year_based_date_related_fields/__init__.py
+from frappe_better_select_control import __version__ as version
+
+setup(
+    name='frappe_better_select_control',
+    version=version,
+    description='Frappe select control that supports options group.',
+    author='Ameen Ahmed (Level Up)',
+    author_email='levelupye@gmail.com, kid1194@gmail.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_requires
+)
