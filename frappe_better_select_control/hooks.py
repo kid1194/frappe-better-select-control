a8283f7965e6268959835b8f2c8984dba3d8fa4d
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-04 19:26:19 +0300

    Update to v1.1.0

--- frappe_better_select_control/hooks.py
+++ frappe_better_select_control/hooks.py
@@ -1,4 +1,5 @@
 from . import __version__ as app_version
+from frappe import __version__ as frappe_version
 
 app_name = "frappe_better_select_control"
 app_title = "Frappe Better Select Control"
@@ -6,8 +7,9 @@ app_publisher = "Ameen Ahmed (Level Up)"
 app_description = "Frappe select control that supports options group."
 app_icon = "octicon octicon-file-directory"
 app_color = "blue"
-app_email = "levelupye@gmail.com, kid1194@gmail.com"
+app_email = "kid1194@gmail.com"
 app_license = "MIT"
+is_frappe_above_v13 = int(frappe_version.split('.')[0]) > 13
 
 # Includes in <head>
 # ------------------
@@ -16,9 +18,7 @@ app_license = "MIT"
 # app_include_css = "/assets/frappe_better_select_control/css/select.css"
 # app_include_js = "/assets/frappe_better_select_control/js/select.js"
 
-app_include_js = [
-    "/assets/frappe_better_select_control/js/better_select.js",
-]
+app_include_js = ['better_select.bundle.js'] if is_frappe_above_v13 else ['/assets/frappe_better_select_control/js/better_select.js']
 
 # include custom scss in every website theme (without file extension ".scss")
 # website_theme_scss = "frappe_better_select_control/public/scss/website"
