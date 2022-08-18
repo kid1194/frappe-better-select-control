4779c4ffa7e3da6909315eefd8e590eda6dbe092
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-18 23:22:18 +0300

    Update to v1.1.3

--- frappe_better_select_control/config/desktop.py
+++ frappe_better_select_control/config/desktop.py
@@ -1,12 +1,12 @@
 from frappe import _
 
 def get_data():
-return [
-    {
-        "module_name": "Frappe Better Select Control",
-        "color": "blue",
-        "icon": "octicon octicon-file-directory",
-        "type": "module",
-        "label": _("Frappe Better Select Control")
-    }
-]
+    return [
+        {
+            "module_name": "Frappe Better Select Control",
+            "color": "blue",
+            "icon": "octicon octicon-file-directory",
+            "type": "module",
+            "label": _("Frappe Better Select Control")
+        }
+    ]
