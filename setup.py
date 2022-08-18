626ddd2e970eb6dfaa0fc07ae74dc9e2db0d209a
Ameen Ahmed <kid_1194@hotmail.com>
2022-08-18 23:21:35 +0300

    Update to v1.1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     install_requires = f.read().strip().split('\n')
 
-# get version from __version__ variable in erpnext_fiscal_year_based_date_related_fields/__init__.py
 from frappe_better_select_control import __version__ as version
 
 setup(
